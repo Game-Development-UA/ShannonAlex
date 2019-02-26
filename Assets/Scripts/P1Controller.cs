1d9fb0e31fa18a050f95bc39bb655e4b5b9de631
Shannon Washington <blendergirl12@gmail.com>
2019-02-26 10:47:04 -0700

    In Class help - BG

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -13,7 +13,10 @@ public class P1Controller : MonoBehaviour
     public Rigidbody2D charac;
     public ParticleSystem blood;
     public Transform spawn;
-    public Transform explosion;
+    public ParticleSystem explosion;
+    //public bool jump1 = false;
+    //public bool jump2= false;
+    public int jump_counter = 0;
 
 
 
@@ -30,12 +33,49 @@ public class P1Controller : MonoBehaviour
         //vertical = Input.GetAxis("Vertical");
         charac.velocity = new Vector2(speed * horizonatal_mvmt, charac.velocity.y);
 
+        //RaycastHit hit;
         if (Input.GetButtonDown("Jump"))
         {
-            if (charac.position.y > 6f)
-                charac.position = charac.position;
-            else
+            /*if (jump1 == false)
+            {
                 charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                jump1 = true;
+            }
+            //charac.position = charac.position;
+            else
+                if (jump2 == false)
+                {
+                    charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                    jump2 = true;
+                }
+                else
+                {
+                    charac.position = charac.position;
+                    jump1 = false;
+                    jump2 = false; //raycasting
+            }
+        */
+            /*if (jump_counter == 0 || jump_counter == 1)
+            {
+                if (Physics.Raycast(transform.position, Vector3.down, 100))
+                {
+                    charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                    jump_counter += 1;
+                }
+                else
+                {
+                    jump_counter += 1;
+                    if (Physics.Raycast(transform.position, transform.forward, 1))
+                    {
+                        jump_counter = 0;
+                    }
+                }
+            }*/
+            if(charac.position.y < 8f)
+            {
+                charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+            }
+
         }
 
     }
@@ -44,6 +84,11 @@ public class P1Controller : MonoBehaviour
         SpeedPowerup moreSpeed = col.gameObject.GetComponent<SpeedPowerup>();
         LifePowerUp moreLife = col.gameObject.GetComponent<LifePowerUp>();
         LifeDecrease lessLife = col.gameObject.GetComponent<LifeDecrease>();
+
+        if (col.gameObject.tag == "endgame")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("EndGame");
+        }
 
         if (moreSpeed != null){speed += moreSpeed.speedIncrease; Destroy(moreSpeed.gameObject);}
 
