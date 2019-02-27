c5cac72bff983525a3e1cf24c75f5fdd2ab98bb3
Drew Castalia <dcastalia@email.arizona.edu>
2019-02-26 21:25:47 -0700

    Added raycast jump logic to main controller
    Also early abort if player is null in camera script.

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -16,69 +16,31 @@ public class P1Controller : MonoBehaviour
     public ParticleSystem explosion;
     //public bool jump1 = false;
     //public bool jump2= false;
-    public int jump_counter = 0;
+    public int maxJumps;
+    int jump_counter;
+    public Transform groundedRaycastOrigin;
+    public float groundedRayLength;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         horizonatal_mvmt = Input.GetAxis("Horizontal");
-        //vertical = Input.GetAxis("Vertical");
         charac.velocity = new Vector2(speed * horizonatal_mvmt, charac.velocity.y);
 
-        //RaycastHit hit;
-        if (Input.GetButtonDown("Jump"))
-        {
-            /*if (jump1 == false)
-            {
-                charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
-                jump1 = true;
-            }
-            //charac.position = charac.position;
-            else
-                if (jump2 == false)
-                {
-                    charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
-                    jump2 = true;
-                }
-                else
-                {
-                    charac.position = charac.position;
-                    jump1 = false;
-                    jump2 = false; //raycasting
-            }
-        */
-            /*if (jump_counter == 0 || jump_counter == 1)
-            {
-                if (Physics.Raycast(transform.position, Vector3.down, 100))
-                {
-                    charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
-                    jump_counter += 1;
-                }
-                else
-                {
-                    jump_counter += 1;
-                    if (Physics.Raycast(transform.position, transform.forward, 1))
-                    {
-                        jump_counter = 0;
-                    }
-                }
-            }*/
-            if(charac.position.y < 8f)
-            {
-                charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
-            }
-
+        RaycastHit2D grounded = Physics2D.Raycast( groundedRaycastOrigin.position, groundedRaycastOrigin.right, groundedRayLength );
+        if( grounded.collider != null ) {
+            if( jump_counter == maxJumps ) jump_counter = 0;
         }
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            if( jump_counter < maxJumps )
+            {
+                charac.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                jump_counter++;
+            }
+        }
     }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         SpeedPowerup moreSpeed = col.gameObject.GetComponent<SpeedPowerup>();
--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -6,8 +6,6 @@ namespace UnityStandardAssets._2D
     public class Camera2DFollow : MonoBehaviour
     {
         public Rigidbody2D player;       //Public variable to store a reference to the player game object
-
-
         private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
         // Use this for initialization
@@ -20,6 +18,8 @@ namespace UnityStandardAssets._2D
         // LateUpdate is called after Update each frame
         void LateUpdate()
         {
+            if( player == null ) return;
+            
             // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
             transform.position = player.transform.position + offset;
         }
